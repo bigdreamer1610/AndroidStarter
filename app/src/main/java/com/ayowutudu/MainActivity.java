19052bd0cc81ae43b620ed96dfc5db9bf6c1cd17
Thuy Nguyen <ayowutudu@gmail.com>
2021-05-13 18:09:37 +0700

    (feat) autolayout, navigation

---
+++ app/src/main/java/com/ayowutudu/MainActivity.java
@@ -0,0 +1,16 @@
+package com.ayowutudu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.inputmethod.InputMethodManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
